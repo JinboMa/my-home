d8706c957125c6c03d2a702a505f781fa37da078
jinboMa <596756194@qq.com>
2017-11-01 20:24:54 +0800

    change style

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -5,21 +5,9 @@
 </template>
 
 <script>
+import '../style/my-header.css'
 import { Vue, Component } from '@/utils/vue-class'
 
 @Component
 export default class MyHeader extends Vue {}
 </script>
-
-<style scoped>
-.mj-header {
-  height: var(--header-height);
-  background: -webkit-gradient(
-    linear,
-    0 0,
-    0 bottom,
-    from(var(--header-color)),
-    to(rgba(0, 0, 0, 0))
-  );
-}
-</style>
